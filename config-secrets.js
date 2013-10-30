dccfc9c613c4841dabb5243f07a15c6d6ad482b0
Jason Pullias <jason@pullias.com>
2013-10-30 11:05:19 -0500

    remove config-secrets from version control

--- config-secrets.js
+++
@@ -1,7 +0,0 @@
-// This file is intended for secret keys that should not be version-controlled
-var secrets = {};
-
-secrets.cookieParserSecret = 'not secret';
-secrets.echoNestKey = 'fake key';
-
-module.exports = secrets;
